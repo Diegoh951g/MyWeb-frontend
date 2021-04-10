95d3e3b1134c58860fc41e4eb906e26e59dbb1e9
Diego Maldonado Zamorano <diego.maldonado.1alsf@gmail.com>
2021-04-09 23:13:28 -0400

    Api integration

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,33 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
   private my_name: string;
   private my_age: number;
   private my_country: string;
 
-  constructor() { 
-
+  constructor() {
     this.my_name = 'Diego Maldonado Zamorano';
     this.my_age = 21;
     this.my_country = 'Chile';
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  getName(): string{
+  getName(): string {
     return this.my_name;
   }
 
-  getAge(): number{
+  getAge(): number {
     return this.my_age;
   }
 
-  getCountry(): string{
+  getCountry(): string {
     return this.my_country;
   }
 }
